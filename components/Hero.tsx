412999f6667cbdab62c1936e1e623412281f48d0
harshedabdulla <harshed2004@gmail.com>
2023-04-15 19:04:43 +0530

    chan

---
+++ components/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Cursor,useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {}
+
+export default function Hero({}: Props) {
+    const [text,count]=useTypewriter({
+        words:[
+            "Hi, The Name's Harshed Abdulla",
+            "Guy-who-loves-gym.tsx",
+            "<ButLovesToCodeMore/>",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center'>
+        <BackgroundCircles />
+        <h1>
+            <span>
+                {text}
+            </span>
+            <Cursor cursorColor='#F7AB0A' /> 
+        </h1>
+    </div>
+  )
+}
