5b63bf567d440f482ec21e5f3f5c7c7a359a3fe9
harshedabdulla <harshed2004@gmail.com>
2023-04-20 23:24:26 +0530

    completed skills

--- components/About.tsx
+++ components/About.tsx
@@ -15,9 +15,7 @@ export default function About({}: Props) {
         opacity:1,
     }}
     className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-            About
-        </h3>
+       
         <motion.img
         initial={{
             x:-200,
@@ -34,16 +32,11 @@ export default function About({}: Props) {
             once:true
         }}
         src='https://harshedabdulla.github.io/portf3/images/pic06.jpg'
-        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+        className='-mb-16 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[450px] xl:h-[500px]'
         />
-
-        <div className='space-y-10 px-0 md:px-10'>
-            <h4 className='text-4xl font-semibold'>
-                Here is a <span className='underline decoration-[#F7AB0A]/50'>little</span> background
-            </h4>
-            <p className='text-base'>My name is Harshed, and I am a web developer with a strong passion for creating and designing web applications that not only look great, but are also functional and user-friendly. I have a natural curiosity and a love for problem-solving, which has driven me to constantly seek out new challenges and learn new technologies in the field of web development.
-
-I am based in Kochi, Ernakulam and am always open to new opportunities and collaborations. If you have any coding problems or web development projects that you'd like to discuss, don't hesitate to reach out! I believe in the power of clean, efficient design and strive to get things done quickly and with attention to detail. I'm looking forward to connecting and working with others who share my passion for web development.</p>
+        <div className='space-y-8 px-2 md:px-10'>
+            
+            <p className='text-base'>My name is Harshed, and I am a web developer with a strong passion for creating and designing web applications that not only look great, but are also functional and user-friendly. I have a natural curiosity and a love for problem-solving, which has driven me to constantly seek out new challenges and learn new technologies in the field of web development.</p>
         </div>
         </motion.div>
   )
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-l items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-l items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-4 hover:opacity-100 opacity-20 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.img
         initial={
             {
@@ -22,10 +22,10 @@ export default function ExperienceCard({}: Props) {
         viewport={{
             once:true
         }}
-        className='w-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+        className='w-32 rounded-full xl:w-[150px] xl:h-[150px] object-cover object-center'
         src="https://images.yourstory.com/cs/images/companies/XenaIntelligence1-1663924243493.jpg?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff" 
         alt="xena intelligence" />
-        <div className='px-0 md:px-10'>
+         <div className="px-0 md:px-10">
             <h4 className='text-4xl font-light'>Student Intern</h4>
             <p className='font-bold text-2xl mt-1'>Xena Intelligence</p>
             <div className='flex space-x-2 my-2'>
@@ -40,13 +40,11 @@ export default function ExperienceCard({}: Props) {
                 src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/187_Js-1024.png" alt="" />
             </div>
             
-            <p className='uppercase py-5 text-gray-500'>Started work...</p>
+            <p className='uppercase py-3 text-gray-500'>Started work...</p>
             <ul className='list-disc space-y-4 ml-5 text-lg'>
                 <li>Summary points</li>
                 <li>Summary points</li>
-                <li>Summary points</li>
-                <li>Summary points</li>
-                <li>Summary points</li>
+               
             </ul>
         </div>
     </article>
